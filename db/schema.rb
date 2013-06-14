48522da8edec5a17885b14f8b5a7e2e93394a79a
Joshua Lee <jsungholee@gmail.com>
2013-06-14 16:43:43 -0400

    data model fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613214602) do
+ActiveRecord::Schema.define(:version => 20130614174952) do
+
   create_table "movie_likes", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
