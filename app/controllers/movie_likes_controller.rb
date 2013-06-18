9c0596d2f0c6264c57e697fb0ef9f6f284feb42c
Eric Kelly <heroiceric@gmail.com>
2013-06-17 22:05:24 -0400

    Small refactor of MovieLikesController
    - Some comments that explain what was going on

--- app/controllers/movie_likes_controller.rb
+++ app/controllers/movie_likes_controller.rb
@@ -1,17 +1,25 @@
 class MovieLikesController < ApplicationController
+  # /movies/:movie_id/movie_likes/new
 	def new
     @movie = Movie.find(params[:movie_id])
     @movie_like = @movie.movie_likes.build
   end
 
 	def create
+    # from the params, we can grab the id of the movie we need
 		@movie = Movie.find(params[:movie_id])
 		@movie_like = @movie.movie_likes.build(params[:movie_like])
-		@movie_like.user_id = current_user.id
+		@movie_like.user = current_user
+
+    # movies_path #=> /movies
+    # movie_path(@movie) #=> /movies/:id_of_movie
+
+    # movie_movie_like_path(@movie, @movie_like)
+    # =>/movies/3/movie_likes/5
 
 		if @movie_like.save
-      flash[:notice] = "You have liked this movie."
-      redirect_to movie_path(@movie)
+      # flash[:notice] = "You have liked this movie."
+      redirect_to movie_path(@movie), notice: "You have liked this movie."
     else
       render action: "new"
    	end
