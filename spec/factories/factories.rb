a64c785cdbadc7a4355fa5364a3de73401890f5a
Alex Espinoza <aespinoza@aspria.net>
2013-06-18 15:18:21 -0400

    Tests and factories refactored

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
-  sequence :movie do |name|
-    "Generic Blockbuster Movie Number #{name}"
+  sequence :movie_title do |n|
+    "Generic Blockbuster Movie Number #{n}"
   end
 
-  sequence :movie_like do |like|
-    "#{like}"
+  sequence :movie_like do |n|
+    "#{n}"
   end
 
   factory :movie do
@@ -16,41 +16,11 @@ FactoryGirl.define do
     association :contributor, factory: :user
   end
 
-  factory :movie_for_like_test1, :class => Movie do
-    title "Pretty Good Movie"
-    year "2013"
-    genre "Documentary"
-    director "Bob Saget"
-    summary "A cop turns to a mystical cat lady to convert into a ninja commando to save the world from a diabolical turtle hellbent on melting Antarctica."
-    association :contributor, factory: :user
-    id "500"
-  end
-
-  factory :movie_for_like_test2, :class => Movie do
-    title "Pretty Bad Movie"
-    year "2013"
-    genre "Documentary"
-    director "Bob Saget"
-    summary "A cop turns to a mystical cat lady to convert into a ninja commando to save the world from a diabolical turtle hellbent on melting Antarctica."
-    contributor_id "1"
-    id "501"
-  end
-
-  factory :movie_for_like_test3, :class => Movie do
-    title "Pretty Even Movie"
-    year "2013"
-    genre "Documentary"
-    director "Bob Saget"
-    summary "A cop turns to a mystical cat lady to convert into a ninja commando to save the world from a diabolical turtle hellbent on melting Antarctica."
-    contributor_id "100"
-    id "502"
-  end
-
   factory :review do
     score "5"
     body "this is the review"
-    movie_id "1"
-    contributor_id "1"
+    association :movie
+    association :contributor, factory: :user
   end
 
   factory :user do
@@ -61,32 +31,8 @@ FactoryGirl.define do
     password_confirmation "123456789"
   end
 
-  factory :user_for_like_test, :class => User do
-    id "100"
-    first_name "a"
-    last_name "e"
-    email "a@a.com"
-    password "123456789"
-    password_confirmation "123456789"
-  end
-
   factory :movie_like do
     association :user
     association :movie
-  end
-
-  factory :movie_like_specific1, :class => MovieLike do
-    user_id "100"
-    movie_id "500"
-  end
-
-  factory :movie_like_specific2, :class => MovieLike do
-    user_id "100"
-    movie_id "501"
-  end
-
-  factory :movie_like_specific3, :class => MovieLike do
-    user_id "100"
-    movie_id "502"
   end
 end
--- spec/features/movie_like_spec.rb
+++ spec/features/movie_like_spec.rb
@@ -3,13 +3,6 @@ require 'spec_helper'
 describe 'Movie Like' do
   let(:user) { FactoryGirl.create(:user) }
 
-	let(:simulate_user_log_in) do
-		visit new_user_session_path
-  	fill_in "Email", with: "a@a.com"
-  	fill_in "Password", with: "123456789"
-  	click_button "Sign in"
-	end
-
 	it "after liking a movie, I will be redirected back to the respective movie's page." do
 		FactoryGirl.create(:movie)
 		sign_in_as(user)
